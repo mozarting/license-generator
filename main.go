90293fdc7ea4e0aa558723eb44c07106ea838829
mahesh-143 <maheshodedara13@yahoo.com>
2025-01-29 19:57:06 +0530

    feat: create LICENSE file

--- main.go
+++ main.go
@@ -22,26 +22,47 @@ func main() {
 		Use:   "add",
 		Short: "Add license in current directory",
 		Run: func(cmd *cobra.Command, args []string) {
+			if license == "" || name == "" || year == "" {
+				fmt.Println("ERROR: 'license', 'name', and 'year' flags are required.")
+				os.Exit(1)
+			}
+
 			// get license files  from directory
 			licenses, err := os.ReadDir("./licenses")
 			if err != nil {
-				fmt.Println("ERROR: reading directory", err)
+				fmt.Println("ERROR: Failed to read 'licenses' directory: ", err)
+				os.Exit(1)
 			}
+			licenseFound := false
 			// create license file
 			for _, l := range licenses {
 				fmt.Println(l.Name())
 				if strings.EqualFold(l.Name(), license) {
+					licenseFound = true
 					file_content, err := os.ReadFile("licenses/" + l.Name())
 					if err != nil {
-						fmt.Println("ERROR: Failed to read file, ", err)
+						fmt.Printf("ERROR: Failed to read file '%s': %v\n", l.Name(), err)
 						return
 					}
 					r := strings.NewReplacer("<year>", year, "<copyright holder>", name)
-					fmt.Println(r.Replace(string(file_content)))
+					fc := r.Replace(string(file_content))
+					err = os.WriteFile("LICENSE", []byte(fc), 0644)
+					if err != nil {
+						fmt.Println("ERROR: Failed to create 'LICENSE' file: ", err)
+						os.Exit(1)
+					}
+
+					fmt.Println("LICENSE file created successfully!")
 					break
 				}
 			}
-			fmt.Printf("License generated!\n license_type : %s\n author_name : %s\n year: %s\n", license, name, year)
+
+			if !licenseFound {
+				fmt.Printf("ERROR: License '%s' not found in './licenses'.\n", license)
+				os.Exit(1)
+			}
+
+			fmt.Printf("License generated!\nLicense Type: %s\nAuthor Name: %s\nYear: %s\n", license, name, year)
 		},
 	}
 
